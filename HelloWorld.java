612775fab8c9f9a8260877d9179ed7c130a764e6
Sarah Mortell <sarahm280396@gmail.com>
2022-10-28 13:53:23 +0100

    First Program 'HelloWorld'

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+package com.itbulls.mortell;
+
+/**
+ * 
+ * Hello World Application 
+ * 
+ * @author Sarah Mortell
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * The entry point to the program.
+	 * 
+	 * @param args input arguments 
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Please do not remove 
+		 * this code.
+		 */
+		System.out.println("Hello World");
+		
+		//TODO add more code here in the future
+	}
+}
